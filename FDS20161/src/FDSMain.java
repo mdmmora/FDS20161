a518e7434d3bfb9cd8f757771e2883222e53f2b9
mcmor <mcmor@10.32.181.87>
2016-06-21 19:15:33 -0300

    Versão inicial.

---
+++ FDS20161/src/FDSMain.java
@@ -0,0 +1,15 @@
+
+public class FDSMain {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World Comunitário!!!\n\n");
+		
+		//Michael
+		System.out.println("Hello, World! By Michael!");
+		
+		//Acrescente, abaixo, um comentário com seu nome e um
+		//     System.out, como acima. 
+
+	}
+
+}
